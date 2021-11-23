c6a03bb9d87430a3e6d84b0e4149513f7eb2764f
asherkuu <asher_kuu@naver.com>
2021-11-23 17:38:02 +0900

    fix: vhost 및 세션 삭제

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import expressSession from "express-session";
 
 const app = express();
 
@@ -12,13 +11,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(
-//   expressSession({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     cookie: { maxAge: 24000 * 60 * 60 },
-//   })
-// );
 
 /*
     Route
